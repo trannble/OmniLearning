705abaee4ee94450a12f7d52f63752d2ba3806f8
Tran L <52722842+trannble@users.noreply.github.com>
2020-04-21 17:24:45 -0700

    Initial Commit

---
+++ OmniLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OmniLearning
+//
+//  Created by Tran Le on 4/21/20.
+//  Copyright © 2020 TL Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
