9303272a2687aa702303359da5854a31847b79ff
Tran L <52722842+trannble@users.noreply.github.com>
2020-04-21 17:55:08 -0700

    Add Initial Background

---
+++ OmniLearning/Controller/WelcomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  OmniLearning
+//
+//  Created by Tran Le on 4/21/20.
+//  Copyright © 2020 TL Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - View Settings
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+    }
+
+
+}
+
