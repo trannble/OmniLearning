c1be63cff6f6950c8173ec1836ca3f687999a719
Tran L <52722842+trannble@users.noreply.github.com>
2020-04-21 23:46:21 -0700

    Restruct Wireframe

--- OmniLearning/Controller/RegisterViewController.swift
+++ OmniLearning/Controller/RegisterViewController.swift
@@ -15,12 +15,23 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var registerError: UILabel!
     @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var userTypePickerView: UIPickerView!
+    
+    fileprivate var pickerData = ["Student", "Mentor"]
+    fileprivate var userType = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.userTypePickerView.delegate = self
+        self.userTypePickerView.dataSource = self
+        
+        //styling
         email.layer.cornerRadius = 20
         email.clipsToBounds = true
+        
+        userTypePickerView.layer.cornerRadius = 30
+        userTypePickerView.clipsToBounds = true
         
         password.layer.cornerRadius = 20
         password.clipsToBounds = true
@@ -35,16 +46,45 @@ class RegisterViewController: UIViewController {
     @IBAction func registerButtonPressed(_ sender: UIButton) {
         
     }
+
+}
+
+//MARK: - Picker View
+
+extension RegisterViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        userType = pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+        var pickerLabel = view as? UILabel;
+        
+        if (pickerLabel == nil)
+        {
+            pickerLabel = UILabel()
 
+            pickerLabel?.font = UIFont(name: "System Thin", size: 30)
+            pickerLabel?.font = UIFont.systemFont(ofSize: 30)
+            pickerLabel?.textAlignment = NSTextAlignment.center
+            pickerLabel?.textColor = UIColor(named: "DarkPurple")
+        }
+
+        pickerLabel?.text = pickerData[row]
+
+        return pickerLabel!;
+    }
 }
